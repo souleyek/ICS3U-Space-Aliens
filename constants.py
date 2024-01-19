4c9b5c93e937213d2f5de4b888a6b0cc789a5490
souleye keita <144372436+souleyek@users.noreply.github.com>
2024-01-19 12:30:46 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,5 +10,25 @@ SCREEN_Y = 128
 SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 5
+SHIP_SPEED = 1
+ALIEN_SPEED = 1
+LASER_SPEED = 2
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up" ,
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
+
+#new pallet for red filled text 
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
